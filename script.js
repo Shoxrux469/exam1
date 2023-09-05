fa1fa7708293d6a14d31ddeb9fc99afae274edc5
Шохрух <138532283+Shoxrux469@users.noreply.github.com>
2023-09-05 23:24:39 +0500

    fso

--- script.js
+++ script.js
@@ -1,192 +1,146 @@
 // ИСХОДНЫЕ ДАННЫЕ НЕ ТРОГАТЬ!*
 
-let successful = []
+let successful = [];
 
-let unSuccessful = []
+let unSuccessful = [];
 
-let taxes = Number
+let taxes = Number;
 
-let taxesMax = {}
+let taxesMax = {};
 
-let taxesMin = {}
+let taxesMin = {};
 
 // Реальные данные 2021*
 
 let bank = [
+  {
+    name: "Apple",
 
-{
+    budget: 1000000,
 
-name: 'Apple',
+    tax: 28,
 
-budget: 1000000,
+    expensesPerYear: [
+      { title: "Salaries", total: 125000 },
 
-tax: 28,
+      { title: "Utilites", total: 18000 },
 
-expensesPerYear: [
+      { title: "Rent", total: 258000 },
+    ],
+  },
 
-{ title: 'Salaries', total: 125000 },
+  {
+    name: "Microsoft",
 
-{ title: 'Utilites', total: 18000, },
+    budget: 988000,
 
-{ title: 'Rent', total: 258000 }
+    tax: 21,
 
-]
+    expensesPerYear: [
+      {
+        title: "Salaries",
 
-},
+        total: 148000,
+      },
 
-{
+      {
+        title: "Utilites",
 
-name: 'Microsoft',
+        total: 124000,
+      },
 
-budget: 988000,
+      {
+        title: "Rent",
 
-tax: 21,
+        total: 314000,
+      },
+    ],
+  },
 
-expensesPerYear: [
+  {
+    name: "HP",
 
-{
+    budget: 609000,
 
-title: 'Salaries',
+    tax: 14,
 
-total: 148000
+    expensesPerYear: [
+      {
+        title: "Salaries",
 
-},
+        total: 414000,
+      },
 
-{
+      {
+        title: "Utilites",
 
-title: 'Utilites',
+        total: 19000,
+      },
 
-total: 124000,
+      {
+        title: "Rent",
 
-},
+        total: 114400,
+      },
+    ],
+  },
 
-{
+  {
+    name: "Xiomi",
 
-title: 'Rent',
+    budget: 889500,
 
-total: 314000
+    tax: 17,
 
-}
+    expensesPerYear: [
+      {
+        title: "Salaries",
 
-]
+        total: 318000,
+      },
 
-},
+      {
+        title: "Utilites",
 
-{
+        total: 14000,
+      },
 
-name: 'HP',
+      {
+        title: "Rent",
 
-budget: 609000,
+        total: 169000,
+      },
+    ],
+  },
 
-tax: 14,
+  {
+    name: "Samsung",
 
-expensesPerYear: [
+    budget: 889500,
 
-{
+    tax: 12,
 
-title: 'Salaries',
+    expensesPerYear: [
+      {
+        title: "Salaries",
 
-total: 414000
+        total: 650400,
+      },
 
-},
+      {
+        title: "Utilites",
 
-{
+        total: 29000,
+      },
 
-title: 'Utilites',
+      {
+        title: "Rent",
 
-total: 19000,
-
-},
-
-{
-
-title: 'Rent',
-
-total: 114400
-
-}
-
-]
-
-},
-
-{
-
-name: 'Xiomi',
-
-budget: 889500,
-
-tax: 17,
-
-expensesPerYear: [
-
-{
-
-title: 'Salaries',
-
-total: 318000
-
-},
-
-{
-
-title: 'Utilites',
-
-total: 14000,
-
-},
-
-{
-
-title: 'Rent',
-
-total: 169000
-
-}
-
-]
-
-},
-
-{
-
-name: 'Samsung',
-
-budget: 889500,
-
-tax: 12,
-
-expensesPerYear: [
-
-{
-
-title: 'Salaries',
-
-total: 650400
-
-},
-
-{
-
-title: 'Utilites',
-
-total: 29000,
-
-},
-
-{
-
-title: 'Rent',
-
-total: 212000
-
-}
-
-]
-
-},
-
-]
+        total: 212000,
+      },
+    ],
+  },
+];
 
 // 1. Высчитать месячные траты, создав ключ expensesPerMonth в объектах*
 
@@ -205,49 +159,44 @@ total: 212000
 // ТРИ ОЦЕНКИ. ЧИСТОТА КОДА, ЛОГИКА РАБОТЫ, УНИКАЛЬНОСТЬ КОДА*
 
 const setup = () => {
-    function arr(x) {
-        taxes = 0
-        for(i = 0; i < x.length; i++) {
-            taxes += x[i]
-        }
-        // console.log(taxes); // 4
+  function arr(x) {
+    taxes = 0;
+    for (i = 0; i < x.length; i++) {
+      taxes += x[i];
     }
-    let tax = []
-    let minMax = []
-    bank.forEach(item => {
-        let a = item.expensesPerYear.reduce((a, b) => a + b.total, 0)
-        item.expensesPerMonth = a / 12
-        
-        
-        let budgetMonth = item.budget / 12
-        let procent = (item.expensesPerMonth * 100) / budgetMonth
-        // console.log(procent + '%'); // 2
-        
-        
-        let sum = budgetMonth - budgetMonth / item.tax
-        if(sum > 0) {
-            successful.push({sum, ...item})
-        } else {
-            unSuccessful.push({sum, ...item})
-        }
-        
-        
-        let payment = budgetMonth / item.tax
-        tax.push(payment)
-        minMax.push({payment, ...item})
-        
-        
-        let totalMoney = item.budget - a - (item.budget / item.tax)
-        bank.push({totalMoney, ...item})
-    })
-    // console.log(bank); // 1, 6
-    // console.log(successful);// 3
-    taxesMax = minMax.reduce((a, b) => a.payment > b.payment ? a : b)
-    taxesMin = minMax.reduce((a, b) => a.payment < b.payment ? a : b)
-    // console.log(taxesMax, taxesMin); // 5
-    arr(tax)
-    // console.log(tax);
-    
-}
+    // console.log(taxes); // 4
+  }
+  let tax = [];
+  let minMax = [];
+  bank.forEach((item) => {
+    let a = item.expensesPerYear.reduce((a, b) => a + b.total, 0);
+    item.expensesPerMonth = a / 12;
 
-setup()
+    let budgetMonth = item.budget / 12;
+    item.procent = (item.expensesPerMonth * 100) / budgetMonth;
+    // console.log(item.procent + '%'); // 2
+
+    let sum = budgetMonth - budgetMonth / item.tax;
+    if (sum > 0) {
+      successful.push({ sum, ...item });
+    } else {
+      unSuccessful.push({ sum, ...item });
+    }
+
+    let payment = budgetMonth * item.tax / 100 
+    tax.push(payment);
+    minMax.push({ payment, ...item });
+
+    let totalMoney = item.budget - a - (item.budget * item.tax) / 100;
+    bank.push({ totalMoney, ...item });
+  });
+//   console.log(bank); // 1, 6
+//   console.log(successful);// 3
+  taxesMax = minMax.reduce((a, b) => (a.payment > b.payment ? a : b));
+  taxesMin = minMax.reduce((a, b) => (a.payment < b.payment ? a : b));
+//   console.log(taxesMax, taxesMin); // 5
+  arr(tax);
+//   console.log(tax);
+};
+
+setup();
